c7858afeaec3f184ad3e8b63f3d9c158939209e1
Amit Kumar <106420527+amitjait@users.noreply.github.com>
2023-02-21 00:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices = document.getElementsByClassName('price');
+	let total = 0;
+    console.log(prices);
+	
+    for(let i=0; i<prices.length; i++){
+        total += (Number)(prices[i].innerHTML);
+    }
+
+	let tr = document.createElement('tr');
+	let td = document.createElement('td');
+
+	td.innerHTML = total;
+	tr.appendChild(td);
+
+
+	let table = document.getElementById('itemTable');
+
+	table.appendChild(tr);
   
 };
 
